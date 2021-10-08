14949ddc09d26fb69e88c912af45211f4674b44f
Owen@Lifelessstudio.com <77976026+Owen7000@users.noreply.github.com>
2021-10-08 12:22:01 +0100

    Start of boot sequence

--- Version 1/LOS.py
+++ Version 1/LOS.py
@@ -1,4 +1,41 @@
-import time
+import time, sys, os
+
+class LOS:
+    def __init__(self) -> None:
+        pass
+    class boot:
+        def __init__(self) -> None:
+            self.ready_load = False
+        def boot_anim(self):
+            load_str = ("Please wait while the system loads all of the required data...")
+            ls_len = len(load_str) 
+            animation = "|/-\\"
+            anicount = 0
+            counttime = 0		
+            i = 0	
+            			
+
+            while (self.ready_load != True): 
+                time.sleep(0.1) 
+                sys.stdout.write("\r"+ load_str + animation[anicount]) 
+                sys.stdout.flush() 
+                anicount = (anicount + 1)% 4
+                i =(i + 1)% ls_len 
+                counttime = counttime + 1
+                if counttime == 100:
+                    self.ready_load = True
+            
+            # for windows OS 
+            if os.name =="nt": 
+                os.system("cls") 
+                
+            # for linux / Mac OS 
+            else: 
+                os.system("clear") 
+
+        def do_boot():
+            boot = LOS.boot()
+            boot.boot_anim()
 
 
-time.sleep(5)
+#LOS.boot.do_boot()
--- Version 1/boot.py
+++ Version 1/boot.py
@@ -1,2 +1,8 @@
+from os import system
+import sys
 import LOS
 
+
+if __name__ == '__main__':
+    LOS.LOS.boot.do_boot()
+    
